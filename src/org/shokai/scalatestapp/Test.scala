eee322733cf651f31ce88b322f6ad1bf439a9a32
Sho Hashimoto <hashimoto@shokai.org>
2013-09-22 23:29:37 +0900

    scalaでLog.vとか文字列内での変数展開とか書いてみた

--- src/org/shokai/scalatestapp/Test.scala
+++ src/org/shokai/scalatestapp/Test.scala
@@ -2,10 +2,14 @@ package org.shokai.scalatestapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 class Test extends Activity{
   override def onCreate(savedInstanceState:Bundle){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+    for(i <- 1.to(10)){
+      Log.v("ScalaTestApp", s"start $i")
+    }
   }
 }
