28f5c47503fe7fa1a9a003afabaf6361430c1cd6
Sho Hashimoto <hashimoto@shokai.org>
2013-09-22 22:04:07 +0900

    create java android project

---
+++ gen/R.java.d
@@ -0,0 +1,8 @@
+/Users/sho/src/android/scala/ScalaTestApp/gen/org/shokai/scalatestapp/R.java \
+ : /Users/sho/src/android/scala/ScalaTestApp/res/drawable-hdpi/ic_launcher.png \
+/Users/sho/src/android/scala/ScalaTestApp/res/drawable-ldpi/ic_launcher.png \
+/Users/sho/src/android/scala/ScalaTestApp/res/drawable-mdpi/ic_launcher.png \
+/Users/sho/src/android/scala/ScalaTestApp/res/drawable-xhdpi/ic_launcher.png \
+/Users/sho/src/android/scala/ScalaTestApp/res/layout/main.xml \
+/Users/sho/src/android/scala/ScalaTestApp/res/values/strings.xml \
+/Users/sho/src/android/scala/ScalaTestApp/bin/AndroidManifest.xml \
