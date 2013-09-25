9ba2da5197c3943d16534a378036ad6595ea052a
Sho Hashimoto <hashimoto@shokai.org>
2013-09-25 19:40:52 +0900

    まとめてimport

--- src/org/shokai/scalatestapp/MainActivity.scala
+++ src/org/shokai/scalatestapp/MainActivity.scala
@@ -3,9 +3,7 @@ package org.shokai.scalatestapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.EditText;
-import android.widget.Button;
+import android.widget.{TextView, EditText, Button};
 import android.view.View;
 import android.view.View.OnClickListener;
 
